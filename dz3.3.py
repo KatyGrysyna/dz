5b5c31203584129028608cebe5f6546e5b0ecf7a
KatyGrysyna <katerinkakc@gmail.com>
2026-02-20 14:01:36 +0200

    add dz3.3

---
+++ dz3.3.py
@@ -0,0 +1,17 @@
+#lst = [1, 2, 3, 4, 5, 6]
+#lst = [1, 2, 3]
+#lst = [1, 2, 3, 4, 5]
+#lst = [1]
+#lst = []
+n = len(lst)
+print(n)
+if n == 0:
+    res = [[], []]
+elif n % 2 == 0:
+    half = n // 2
+    res = [lst[:half], lst[half:]]
+else:
+    half = n // 2 + 1
+    res = [lst[:half], lst[half:]]
+
+print(res)
