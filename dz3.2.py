ed8a7f98558974c51ba965455845013c1721d2ea
KatyGrysyna <katerinkakc@gmail.com>
2026-02-20 13:23:24 +0200

    edit dz3.2

--- dz3.2.py
+++ dz3.2.py
@@ -1,11 +1,7 @@
 #lst = [12, 3, 4, 10]
 #lst = [1]
 #lst = []
-#lst = [12, 3, 4, 10, 8]
-
-if len(lst) >1:
-    last = lst[-1]
-    lst1 = lst[:-1]
-    lst = [last] + lst1
+lst = [12, 3, 4, 10, 8]
+lst = lst[-1:] + lst[:-1]
 print(lst)
 
