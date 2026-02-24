18736af43ebd10f1ddb1215c7a27be0943d45761
KatyGrysyna <katerinkakc@gmail.com>
2026-02-24 14:16:17 +0200

    editt dz4.3

--- dz4.3.py
+++ dz4.3.py
@@ -1,8 +1,10 @@
 import random
+#num = []
+#for i in range(random.randint(3, 10)):
+    #num.append(random.randint(1, 100))
+#res = [num[0], num[2], num[-2]]
 
-num = []
-for i in range(random.randint(3, 10)):
-    num.append(random.randint(1, 100))
+num = [random.randint(3, 100) for _ in range(random.randint(3, 10))]
 res = [num[0], num[2], num[-2]]
 print(num)
 print(res)
