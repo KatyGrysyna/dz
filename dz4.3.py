7f49473723d3f1df5da2c8e8a6f656bc1a3a6aa9
KatyGrysyna <katerinkakc@gmail.com>
2026-02-24 14:18:00 +0200

    edit3 dz4.3

--- dz4.3.py
+++ dz4.3.py
@@ -1,9 +1,5 @@
 import random
-#num = []
-#for i in range(random.randint(3, 10)):
-    #num.append(random.randint(1, 100))
-#res = [num[0], num[2], num[-2]]
-
+#оптимальний варіант рішення
 num = [random.randint(3, 100) for _ in range(random.randint(3, 10))]
 res = [num[0], num[2], num[-2]]
 print(num)
