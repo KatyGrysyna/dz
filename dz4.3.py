7dff1226efd72464bc6806190dadc92b1b55e069
KatyGrysyna <katerinkakc@gmail.com>
2026-02-24 13:58:21 +0200

    add dz4.3

---
+++ dz4.3.py
@@ -0,0 +1,11 @@
+import random
+from typing import final
+
+lst = random.randint(3,10)
+num = []
+for i in range(lst):
+    num.append(random.randint(0, 100))
+final = [num[0], num[2], num[-2]]
+print(num)
+print(final)
+
