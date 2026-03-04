c7c3902750beca3527048dc7a3d855905f3fbf60
KatyGrysyna <katerinkakc@gmail.com>
2026-03-04 13:38:42 +0200

    add dz5.2

---
+++ dz5.2.py
@@ -0,0 +1,25 @@
+while True:
+    x = int(input('Введіть перше число:'))
+    y = int(input('Введіть друге число:'))
+    z = input('Введіть дію над числами(+, -, *, /):')
+    if z == "+":
+        print(x + y)
+    elif z == "-":
+        print(x - y)
+    elif z == "*":
+        print(x * y)
+    elif z == "/":
+        if y == 0:
+            print('Ділення на 0 неможливе')
+        else:
+            print(x / y)
+    else:
+        print('Невірна дія')
+    answer = input("Продовжити роботу? (y/n):")
+
+    if answer != "y":
+        print("Роботу завершено")
+        break
+
+
+
