16ae9e56443013d78bce384ba5ac8a65f14f16d7
KatyGrysyna <katerinkakc@gmail.com>
2026-03-04 14:03:52 +0200

    edit dz5.3

--- dz5.3.py
+++ dz5.3.py
@@ -5,8 +5,7 @@ for ch in string.punctuation:
     text = text.replace(ch, "")
 words = text.title().split()
 hashtag = "#" + "".join(words)
-if len(hashtag) > 140:
-    hashtag = hashtag[:140]
+hashtag = hashtag[:140]
 print(hashtag)
 
 
