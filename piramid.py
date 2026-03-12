2fa97f8b3f35b9fe913c97f10d7e234edc371953
KatyGrysyna <katerinkakc@gmail.com>
2026-03-12 16:01:07 +0200

    add pyramid

---
+++ piramid.py
@@ -0,0 +1,18 @@
+pyramid = [
+    [7],
+    [5, 8],
+    [9, 8, 2],
+    [1, 3, 5, 6],
+    [6, 2, 4, 4, 5],
+    [9, 5, 3, 5, 5, 7],
+    [7, 4, 6, 4, 7, 6, 8]
+]
+
+def find_max_gold(data):
+    for r in range(len(pyramid) -2, -1, -1):
+        for c in range(len(pyramid[r])):
+            pyramid[r][c] += max(pyramid[r+1][c], pyramid[r+1][c+1])
+    return pyramid[0][0]
+result = find_max_gold(pyramid)
+
+print(f"Максимальна к-ть золота: {result}")
